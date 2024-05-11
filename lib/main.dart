44acf61ed0132be0ae62f7d3c02d13685c266fc2
Sinojon205 <Sinojon205@gmail.com>
2024-05-11 16:53:13 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:js';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/login_view.dart';
+import 'package:mynotes/register_view.dart';
+
+import 'firebase_options.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: false,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                final isEmailVerified = user?.emailVerified ?? false;
+                if (isEmailVerified) {
+                  return
+                } else {
+                  return
+                }
+              default:
+                return Center(child: Text('Loading...'),);
+            }
+          }),
+    );
+  }
+}
